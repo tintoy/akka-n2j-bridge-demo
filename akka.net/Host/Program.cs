0a03acf2613da2518cc9ef56c42950f0e1144261
tintoy <tintoy@tintoy.io>
2015-10-31 15:16:21 +1100

    Add .NET host.

---
+++ akka.net/Host/Program.cs
@@ -0,0 +1,60 @@
+﻿using Akka.Actor;
+using Serilog;
+using System;
+using System.Threading;
+
+namespace Akka.N2J.Host
+{
+	/// <summary>
+	///		Host for the Akka.NET side of the JVM / .NET Akka bridge.
+	/// </summary>
+	static class Program
+	{
+		/// <summary>
+		///		The main program entry-point.
+		/// </summary>
+		static void Main()
+		{
+			SynchronizationContext.SetSynchronizationContext(
+				new SynchronizationContext()
+			);
+
+			ConfigureLogging();
+			try
+			{
+				using (ActorSystem actorSystem = ActorSystem.Create("Akka-CLR"))
+				{
+					Log.Information("Actor system started (press enter to terminate).");
+					Console.ReadLine();
+
+					// Forehead goes here.
+
+					actorSystem.Shutdown();
+					actorSystem.AwaitTermination();
+				}
+				Log.Information("Shutdown complete.");
+			}
+			catch (Exception unexpectedError)
+			{
+				Log.Error(unexpectedError, "Unexpected error: {ErrorMessage}", unexpectedError.Message);
+			}
+        }
+
+		/// <summary>
+		///		Configure the global application logger.
+		/// </summary>
+		static void ConfigureLogging()
+		{
+			const string logTemplate = "[{Level}] {Message}{NewLine}{Exception}";
+
+			Log.Logger =
+				new LoggerConfiguration()
+					.MinimumLevel.Information()
+					.Enrich.WithProcessId()
+					.Enrich.WithThreadId()
+					.WriteTo.Trace(outputTemplate: logTemplate)
+					.WriteTo.LiterateConsole(outputTemplate: logTemplate)
+					.CreateLogger();
+		}
+	}
+}
---
+++ akka.net/Host/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Akka.N2J.Host")]
+[assembly: AssemblyDescription("CLR host for Akka N2J bridge")]
+[assembly: AssemblyCompany("tintoy")]
+[assembly: AssemblyProduct("Akka N2J bridge")]
+[assembly: AssemblyCopyright("Copyright © tintoy 2015")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("85e39318-d040-4e8d-a245-1249cf9e6c87")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
